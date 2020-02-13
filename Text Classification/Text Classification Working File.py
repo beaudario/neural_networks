b6ef31759624d57d4839de629bcab6f623b54bd9
beaudario <dario.douma@upcmail.nl>
2020-02-13 14:14:28 +0100

    lesson 8 12:38 neural networks

--- Text Classification/Text Classification Working File.py
+++ Text Classification/Text Classification Working File.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 
 data = keras.datasets.imdb
 
-(trainData, trainLabels), (testData, testLabels) = data.load_data(num_words=10000)
+(trainData, trainLabels), (testData, testLabels) = data.load_data(num_words=88000)
 
 print(trainData[0])
 
@@ -26,10 +26,11 @@ def decodeReview(text):
     return " ".join([reverseWordIndex.get(i, "?") for i in text])
 
 
+"""
 # model down here
 
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.Embedding(88000, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
@@ -47,7 +48,38 @@ yTrain = trainLabels[10000:]
 fitModel = model.fit(xTrain, yTrain, epochs=40, batch_size=512, validation_data=(xVal, yVal), verbose=1)
 
 results = model.evaluate(testData, testLabels)
+print(results)
 
+model.save("model.h5")
+"""
+
+
+def reviewEncode(s):
+    encoded = [1]
+
+    for word in s:
+        if word.lower() in wordIndex:
+            encoded.append(wordIndex[word.lower()])
+        else:
+            encoded.append(2)
+
+    return encode
+
+
+model = keras.models.load_model("model.h5")
+
+with open("testReview.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nLine = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "")\
+            .replace('"', '').strip()
+        encode = reviewEncode(nLine)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=wordIndex["<PAD>"], padding="post", maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
+
+"""
 testReview = testData[0]
 predict = model.predict(testReview)
 
@@ -56,3 +88,4 @@ print(decodeReview(testReview))
 print("Prediction: ", str(predict[0]))
 print("Actual: ", str(testLabels[0]))
 print(results)
+"""
