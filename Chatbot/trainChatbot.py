0dc3b4743751e139923a3ca21ebdcfeda591ba45
beaudario <dario.douma@upcmail.nl>
2020-02-14 17:40:53 +0100

    create chatbot model

--- Chatbot/trainChatbot.py
+++ Chatbot/trainChatbot.py
@@ -71,12 +71,31 @@ for doc in documents:
 
     training.append([bag, outputRow])
 
-    # shuffle our features and turn into np.array
-    random.shuffle(training)
-    training = np.array(training)
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
 
-    # create train and test lists. X - patterns, Y - intents
-    trainX = list(training[:, 0])
-    trainY = list(training[:, 1])
+# create train and test lists. X - patterns, Y - intents
+trainX = list(training[:, 0])
+trainY = list(training[:, 1])
 
-    print("Training data created")
+print("TRAINING DATA CREATED")
+
+# create model - 3 layers. first layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of
+# neurons equal to number of intents to predict output intent of softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(trainX[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(trainY[0]), activation='softmax'))
+
+# compile model. Stochatic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# fitting and saving the model
+hist = model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("MODEL CREATED")
